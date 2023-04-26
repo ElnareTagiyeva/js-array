4f173c6bc113656141b9c666d67de826018139c0
ElnareTagiyeva <746C077@code.edu.az>
2023-04-26 17:52:10 +0400

    js remove array

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function  removeNum () {
+    var array=[2,3,67,8,45,3];
+this.remove=function(num){
+    array.length--
+    return array;
+}
+}
+var array1= new removeNum();
+var array2=new removeNum();
+console.log(array1.remove());
